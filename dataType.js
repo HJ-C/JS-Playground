2215731602b503470c2606efd4b6ec5067071a95
HJ-C <swch56@naver.com>
2022-03-15 23:45:47 +0900

    #6

---
+++ dataType.js
@@ -0,0 +1,11 @@
+// var 변수 = 1234 ; // primitive 
+// var 어레이 = [1,2,3]; //reference
+// var obj = {name : 'kim'} // reference
+// array, obj는 값이 저장되는것이 아닌 값이 있다는 화살표다. 즉 변수에 값이 저장이 안된다
+
+// let 이름1 = {name : "kIM"}
+
+// function 변경(obj){
+//     obj.name = "Choi"
+// }
+// 변경(이름1)
