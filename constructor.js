0962871a456e03e7d9d7a3c59f18edcae034a2fc
HJ-C <swch56@naver.com>
2022-03-21 23:47:21 +0900

    prototype

--- constructor.js
+++ constructor.js
@@ -8,15 +8,18 @@
 
 // 학생1.sayHi()
 
-function 기계(이름) {
-    this.name = 이름
-    this.age = 15
-    this.sayHi = function(){
-        console.log(`안녕하세요 ${this.name}입니다`)
-    }
-}
+// function 기계(이름) {
+//     this.name = 이름
+//     this.age = 15
+//     this.sayHi = function(){
+//         console.log(`안녕하세요 ${this.name}입니다`)
+//     }
+// }
+// 기계.prototype.gender = '남'
 
-var 학생1 = new 기계('현준')
-var 학생2 = new 기계()
+// var 학생1 = new 기계('현준')
+// var 학생2 = new 기계()
 
-학생1.sayHi()
+// // prototype = key와 value 값을 추가하면 학생1 or 학생2에 gender가 없어도 사용가능
+
+// console.log(학생1.gender)
